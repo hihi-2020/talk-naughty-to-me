7cbb4c89642cd233a2a1312776f6a4d174ece1eb
student <student@eda-bc-07.localdomain>
2020-11-13 11:00:36 +1300

    initial css commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <div className='container'>
+      <div className='navBar'>
+        <button className='top10Button'>Top10!</button>
+      </div>
+      <div className='name'>
+        <label htmlFor="name">
+        <input type="text" name='name' placeholder='Type your name here'/>
+        </label>
+      </div>
+      <div className='getInsult'>
+        <button className='insultButton'>
+          Insult me!
+        </button>
+      </div>
+     
+    </div>
   )
 }
 
